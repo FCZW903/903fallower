c15b5f97467915f6e8254591a3934fbc3ec56186
FCZW903 <iqmemoiq67@gmail.com>
2025-06-02 03:27:07 +0300

    Update log.php

--- log.php
+++ log.php
@@ -1,25 +1,6 @@
 <?php
-function getUserIP() {
-    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-        return $_SERVER['HTTP_CLIENT_IP'];
-    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        return explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'])[0];
-    } else {
-        return $_SERVER['REMOTE_ADDR'];
-    }
-}
-
-$ip = getUserIP();
+$ip = $_SERVER['REMOTE_ADDR'];
 $time = date("Y-m-d H:i:s");
-
-// سطر الحفظ
-$logEntry = "[$time] - IP: $ip\n";
-
-// اسم الملف
-$logFile = 'log.txt';
-
-// حفظ السطر داخل الملف
-file_put_contents($logFile, $logEntry, FILE_APPEND);
-
-echo "✅ تم تسجيل دخول جديد";
+file_put_contents("log.txt", "[$time] - IP: $ip\n", FILE_APPEND);
+echo "تم تسجيل IP: $ip";
 ?>
